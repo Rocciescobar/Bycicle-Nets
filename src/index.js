8a6bab7dd6551a1a6fb78aeceeb0bb4417598664
Manuela <fmanuela499@gmail.com>
2018-02-08 01:45:28 -0500

    funcionalidad autocompletado y trazado de rutas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+
 let bikeMarker;
 
 function initMap() {
-  let location = {lat: -10.0000000, lng: -76.0000000};
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  let location = {lat: -10.0000000,
+    lng: -76.0000000};
   let map = new google.maps.Map(document.getElementById('map'), {
     zoom: 5,
     center: location
   });
 
+  directionsDisplay.setMap(map);
   // Añade marcador
   let myMarker = new google.maps.Marker({
     position: location,
@@ -29,7 +34,8 @@ function initMap() {
     // Añade marcador de bicicleta
     let image = 'assets/images/bike.png';
     bikeMarker = new google.maps.Marker({
-      position: {lat: myLatit, lng: myLongit},    
+      position: {lat: myLatit,
+        lng: myLongit},    
       map: map,
       draggable: true,
       animation: google.maps.Animation.DROP,
@@ -38,12 +44,42 @@ function initMap() {
     bikeMarker.addListener('click', toggleBounce);
 
     map.setZoom(17);
-    map.setCenter({lat: myLatit, lng: myLongit });
+    map.setCenter({lat: myLatit,
+      lng: myLongit });
   };
-
   let functionError = (error) => {
     alert('Tenemos un problema con encontrar tu ubicación');
   };
+
+  // autocompletado:
+  let startPoint = document.getElementById('start-point');
+  let finalPoint = document.getElementById('final-point');
+  new google.maps.places.Autocomplete(startPoint);
+  new google.maps.places.Autocomplete(finalPoint);
+
+  // obtener ruta:
+  let getRoute = (event) => {
+    event.preventDefault();
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  document.getElementById('route').addEventListener('click', getRoute);
+}
+
+let calculateAndDisplayRoute = (directionsService, directionsDisplay) => {
+  directionsService.route({
+    origin: document.getElementById('start-point').value,
+    destination: document.getElementById('final-point').value,
+    travelMode: 'DRIVING'
+  }, function(response, status) {
+    if (status === 'OK') {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Estamos teniendo inconvenientes para encontrar su ubicación');
+    }
+    document.querySelector('.description-js').innerHTML = 'Punto de origen :' + document.getElementById('start-point').value + '<br>' + 'Punto de llegada :' + document.getElementById('final-point').value ;
+    document.getElementById('start-point').value = '';
+    document.getElementById('final-point').value = '';
+  });
 };
 
 // Animación del marcador
@@ -54,3 +90,5 @@ function toggleBounce() {
     bikeMarker.setAnimation(google.maps.Animation.BOUNCE);
   }
 };
+
+
